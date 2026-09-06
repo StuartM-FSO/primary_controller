899c28c0d56fc2c91743a58c0482d2c16a526d2b
StuartM-FSO <stuartm.rec@gmail.com>
2026-09-06 17:35:18 +0100

    Rename adc_hal

--- primary_controller.ino
+++ primary_controller.ino
@@ -1,6 +1,6 @@
 #include "system_state.h"
 #include "time_helpers.h"
-#include "adc-hal.h"
+#include "adc_hal.h"
 
 constexpr uint32_t FREQUENCY_CELL_READ_MS = 250U;
 constexpr uint32_t FREQUENCY_MAIN_LED_FLASH = 1000U;
