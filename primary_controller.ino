f7d293ad734ed3550e74b34aaee40796336b91ad
StuartM-FSO <stuartm.rec@gmail.com>
2026-09-06 22:22:05 +0100

    Move adc health check and led flash to scheduler

--- primary_controller.ino
+++ primary_controller.ino
@@ -43,33 +43,16 @@ void loop() {
     // Handle error
   }
 
-  if(has_timer_elapsed(now, loop_state.main_led_flash_time, FREQUENCY_MAIN_LED_FLASH)){ // Turn main led on & off every 1s
-    bool led_on = !loop_state.main_led_on;
-    digitalWrite(LED_BUILTIN, led_on);
-    system_set_main_led_on(led_on);
-    if(system_set_main_led_timer(now) != STATE_OK){
-      Serial.println("Error writing main led timer");
-      for(;;);
-      // Handle error
-    }
+  if(!scheduler_led_flash(now, loop_state.main_led_flash_time, loop_state.main_led_on)){
+    Serial.println("LED flash failure");
+    for(;;);
+    // Handle error
   }
 
-  if(has_timer_elapsed(now, loop_state.adc_function_check_time, FREQUENCY_ADC_CHECK_MS)){   // Check if ADC is online once a second
-    hal_adc_status_t current_adc_status = adc_health_check();
-    bool adc_online = (current_adc_status == ADC_STATUS_OK);
-
-    if(adc_online){
-      Serial.println("ADC online");
-    } else {
-      Serial.println("ADC offline");
-    }
-
-    if(system_set_adc_online(adc_online) != STATE_OK){
-      Serial.println("Error writing adc_onine");
-      for(;;);
-      // Handle error
-    }
-    system_set_adc_function_check_time(now);
+  if(!scheduler_adc_health_check(now, loop_state.adc_function_check_time)){
+    Serial.println("ADC health check scheduler failed");
+    for(;;);
+    // Handle error
   }
 
   switch (loop_state.current_state) {
@@ -87,6 +70,10 @@ void loop() {
   }
 }
 
+// 00 - WIP
+
+
+
 // 01 - FSM handlers
 
 void fsm_start_up(void){
@@ -103,4 +90,41 @@ void fsm_dive_mode(void){
 
 void fsm_read_cells(void){
 
+}
+
+
+// 02 - Scheduler functions
+
+bool scheduler_adc_health_check(const uint32_t now, const uint32_t elapsed_time){
+  if(has_timer_elapsed(now, elapsed_time, FREQUENCY_ADC_CHECK_MS)){   // Check if ADC is online once a second
+    hal_adc_status_t current_adc_status = adc_health_check();
+    bool adc_online = (current_adc_status == ADC_STATUS_OK);
+
+    if(adc_online){
+      Serial.println("ADC online");
+    } else {
+      Serial.println("ADC offline");
+    }
+
+    if(system_set_adc_online(adc_online) != STATE_OK){
+      Serial.println("Error writing adc_onine");
+      return false;
+    }
+    system_set_adc_function_check_time(now);
+  }
+  return true;
+}
+
+bool scheduler_led_flash(const uint32_t now, const uint32_t elapsed_time, const bool system_led_state){
+  if(has_timer_elapsed(now, elapsed_time, FREQUENCY_MAIN_LED_FLASH)){ // Turn main led on & off every 1s
+    bool led_on = !system_led_state;
+    digitalWrite(LED_BUILTIN, led_on);
+    system_set_main_led_on(led_on);
+    if(system_set_main_led_timer(now) != STATE_OK){
+      Serial.println("Error writing main led timer");
+      return false;
+      // Handle error
+    }
+  }
+  return true;
 }
